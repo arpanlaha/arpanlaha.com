bc577fe9909a6f782868076b273df8ca73f8547d
Arpan Laha <arpanlaha99@gmail.com>
2019-07-24 10:11:28 -0700

    remove redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,22 @@
 import React, { ErrorInfo } from "react";
-import App, { Container, AppInitialProps } from "next/app";
-import { Provider } from "react-redux";
-import withRedux from "next-redux-wrapper";
-import configureStore from "../redux/configureStore";
-import { Store } from "redux";
+import App, { Container } from "next/app";
 // @ts-ignore @types/next-page-transitions does not exist
 import { PageTransition } from "next-page-transitions";
 
-interface AppProps extends AppInitialProps {
-  store: Store;
-}
-
-export default withRedux(configureStore)(
-  class MyApp extends App<AppProps> {
-    componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-      console.error("Page Error Boundary: ", error);
-      super.componentDidCatch(error, errorInfo);
-    }
-
-    render(): JSX.Element {
-      const { Component, pageProps, store } = this.props;
-      return (
-        <Container>
-          <Provider store={store}>
-            <PageTransition timeout={300} classNames="page-transition">
-              <Component {...pageProps} />
-            </PageTransition>
-          </Provider>
-        </Container>
-      );
-    }
+export default class MyApp extends App {
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error("Page Error Boundary: ", error);
+    super.componentDidCatch(error, errorInfo);
   }
-);
+
+  render(): JSX.Element {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component {...pageProps} />
+        </PageTransition>
+      </Container>
+    );
+  }
+}
