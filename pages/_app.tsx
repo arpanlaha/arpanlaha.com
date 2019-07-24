c526403ab13751a408346d45d81b99daba36a605
Arpan Laha <arpanlaha99@gmail.com>
2019-07-23 23:14:45 -0700

    move out of src

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,24 @@
 import React, { ErrorInfo } from "react";
-import App, { Container, AppContext, AppInitialProps } from "next/app";
+import App, { Container, AppInitialProps } from "next/app";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
 import configureStore from "../redux/configureStore";
+import { Store } from "redux";
 // @ts-ignore @types/next-page-transitions does not exist
 import { PageTransition } from "next-page-transitions";
 
-export default withRedux(configureStore)(
-  class MyApp extends App {
-    static async getInitialProps({
-      Component,
-      ctx
-    }: AppContext): Promise<AppInitialProps> {
-      return {
-        pageProps: {
-          // Call page-level getInitialProps
-          ...(Component.getInitialProps
-            ? await Component.getInitialProps(ctx)
-            : {})
-        }
-      };
-    }
+interface AppProps extends AppInitialProps {
+  store: Store;
+}
 
+export default withRedux(configureStore)(
+  class MyApp extends App<AppProps> {
     componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
       console.error("Page Error Boundary: ", error);
-      // @ts-ignore This is needed to render errors correctly in development / production
       super.componentDidCatch(error, errorInfo);
     }
 
     render(): JSX.Element {
-      // @ts-ignore
       const { Component, pageProps, store } = this.props;
       return (
         <Container>
