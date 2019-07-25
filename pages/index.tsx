dce956abc63d3a0f4adb41b35ec5040992dd3313
Arpan Laha <arpanlaha99@gmail.com>
2019-07-25 15:17:17 -0700

    add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { Placeholder } from "../components";
+import Head from "next/head";
 import "../static/style.css";
 
 class App extends Component {
   render(): JSX.Element {
     return (
       <div className="App">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="description" content="Arpan Laha's personal website" />
+          <meta name="keywords" content="Arpan, Laha, Arpan Laha" />
+          <meta name="author" content="Arpan Laha" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Home | Arpan Laha</title>
+        </Head>
         <Placeholder />
       </div>
     );
