47bf7b5167a58fac2f24c51bbfef119bf3ec1915
Arpan Laha <arpanlaha99@gmail.com>
2019-07-28 20:31:44 -0700

    language -> lang

--- components/Head.tsx
+++ components/Head.tsx
@@ -16,7 +16,7 @@ class Head extends Component<HeadProps> {
         <meta name="keywords" content={this.props.keywords || ""} />
         <meta name="author" content="Arpan Laha" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="language" content="English" />
+        <meta name="lang" content="English" />
         <title>
           {`${this.props.title ? `${this.props.title} | ` : ""}Arpan Laha`}
         </title>
