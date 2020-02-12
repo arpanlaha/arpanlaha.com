8c7e2ff68e00f21aeaa7ad72f2c9e06d98923b22
Arpan Laha <arpanlaha99@gmail.com>
2020-02-12 00:06:38 +0000

    remove next-sass hack and update config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
 /* eslint-disable */
-// workaround from https://github.com/zeit/next-plugins/issues/392#issuecomment-475845330
 const withSass = require("@zeit/next-sass");
 
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  config.module.rules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
-        if (u.loader === "css-loader" && u.options) {
-          delete u.options.minimize;
-        }
-      });
-    }
-  });
-}
-
-module.exports = withSass({
-  webpack(config) {
-    HACK_removeMinimizeOptionFromCssLoaders(config);
-    return config;
-  }
-});
+module.exports = withSass({});
