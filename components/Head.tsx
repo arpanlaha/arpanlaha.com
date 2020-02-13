395312e777bfcdc5e415636ccb49056b56da1958
Arpan Laha <arpanlaha99@gmail.com>
2020-02-13 15:07:53 +0000

    add reactstrap css link

--- components/Head.tsx
+++ components/Head.tsx
@@ -22,6 +22,12 @@ export default class Head extends Component<HeadProps> {
         />
         <meta name="author" content="Arpan Laha" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
         <title>
           {`${this.props.title ? `${this.props.title} | ` : ""}Arpan Laha`}
         </title>
