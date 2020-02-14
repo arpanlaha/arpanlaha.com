aa816c76d4d9ea4057891374b0253c019ba51e4e
Arpan Laha <arpanlaha99@gmail.com>
2020-02-14 03:35:06 +0000

    remove redundant stylesheet link

--- components/Head.tsx
+++ components/Head.tsx
@@ -19,11 +19,6 @@ const Head: FunctionComponent<HeadProps> = (props: HeadProps): ReactElement => {
       <meta name="keywords" content={keywords || "Arpan, Laha, Arpan Laha"} />
       <meta name="author" content="Arpan Laha" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap"
-        rel="stylesheet"
-        type="text/css"
-      />
       <title>{`${title ? `${title} | ` : ""}Arpan Laha`}</title>
     </NextHead>
   );
