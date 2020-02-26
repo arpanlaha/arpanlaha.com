5ec1cf99342186ac30c6e1df496c8ad363a9a675
Arpan Laha <arpanlaha99@gmail.com>
2020-02-26 05:14:08 +0000

    start on dark

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,30 +1,33 @@
 import React, { FunctionComponent, ReactElement } from "react";
-import { Icon, Menu } from "antd";
+import { Icon, Layout, Menu } from "antd";
+
+const { Header } = Layout;
 
 const NavBar: FunctionComponent = (): ReactElement => (
-  <Menu mode="horizontal" className="navbar">
-    <Menu.Item key="home">Arpan Laha</Menu.Item>
-    <Menu.Item key="github">
-      <a
-        href="https://github.com/arpanlaha"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon type="github" theme="filled" />
-        GitHub
-      </a>
-    </Menu.Item>
-    <Menu.Item key="linkedin">
-      <a
-        href="https://linkedin.com/in/arpanlaha"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon type="linkedin" theme="filled" />
-        LinkedIn
-      </a>
-    </Menu.Item>
-  </Menu>
+  <Header className="navbar"></Header>
+  // <Menu mode="horizontal" className="navbar">
+  //   <Menu.Item key="home">Arpan Laha</Menu.Item>
+  //   <Menu.Item key="github">
+  //     <a
+  //       href="https://github.com/arpanlaha"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       <Icon type="github" theme="filled" />
+  //       GitHub
+  //     </a>
+  //   </Menu.Item>
+  //   <Menu.Item key="linkedin">
+  //     <a
+  //       href="https://linkedin.com/in/arpanlaha"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       <Icon type="linkedin" theme="filled" />
+  //       LinkedIn
+  //     </a>
+  //   </Menu.Item>
+  // </Menu>
 );
 
 export default NavBar;
