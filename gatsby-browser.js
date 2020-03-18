0bf958875e5a6f95a3c2a7e475b492b08bd5ba12
Arpan Laha <arpanlaha99@gmail.com>
2020-03-18 03:52:36 +0000

    remove antd

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,1 @@
-import "./src/styles/antd.scss";
 import "./src/styles/style.scss";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import React, { ReactElement } from "react";
 import { Head, Placeholder } from "../components";
-import { Button } from "antd";
 
 export default function Home(): ReactElement {
   return (
     <div className="App">
       <Head title="Home" />
       <Placeholder />
-      <Button>Hello</Button>
+      <button>Hello</button>
     </div>
   );
 }
