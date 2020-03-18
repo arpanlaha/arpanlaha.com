b57a1380bb27a11555df3c9ace8c6a5e33e10c9d
Arpan Laha <arpanlaha99@gmail.com>
2020-03-18 04:43:09 +0000

    set up manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
+/* eslint-disable */
+
 module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Arpan Laha",
+        short_name: "Arpan Laha",
+        description: "Arpan Laha's personal website",
+        start_url: "/",
+        background_color: "#abcdef",
+        theme_color: "#fedcba",
+        display: "standalone",
+        icon: "static/favicon.ico"
+      }
+    },
     {
       resolve: "gatsby-plugin-typescript",
       options: {
