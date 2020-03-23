eca92171d484124f1a5297a3d648639437d301ff
Arpan Laha <arpanlaha99@gmail.com>
2020-03-23 00:53:51 +0000

    redirects

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,28 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { Head } from "../components";
 
 import "../styles/style.scss";
 
 export default function ErrorPage(): ReactElement {
+  useEffect((): void => {
+    console.log("hello");
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    }
+
+    const design = localStorage.getItem("design");
+    if (design === "flat") {
+      ["error-message", "return"]
+        .map((className) =>
+          Array.from(document.getElementsByClassName(className))
+        )
+        .forEach((neuClass) =>
+          neuClass.forEach((neuElement) => neuElement.classList.add("flat"))
+        );
+    }
+  }, []);
+
   return (
     <>
       <Head title="Page not found" />
