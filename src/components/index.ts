f0c0356e5eeeb749660ae11645c70b39e35c676e
Arpan Laha <arpanlaha99@gmail.com>
2020-06-30 19:56:52 +0000

    restructure

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import H4IProject from "./H4IProject";
-import Head from "./Head";
-import SideProject from "./SideProject";
-import Social from "./Social";
-
-export { H4IProject, Head, SideProject, Social };
